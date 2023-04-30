e04d460b48aa1b085b69e91526793af45ebde7e1
NeerajaNeelima <023neeraja@gmail.com>
2023-04-30 14:25:57 +0530

    database changed

--- rev/models.py
+++ rev/models.py
@@ -10,12 +10,7 @@ class Revaluation(models.Model):
     Mobile=models.CharField(max_length=10)
     Dhondi_id=models.IntegerField()
     Amount=models.IntegerField()
-    Internal_marks=models.IntegerField()
-    External_marks=models.IntegerField()
     
-    Credits=models.IntegerField()
-    Grades=models.CharField(max_length=2)
-    Revaluation_Status=models.CharField(max_length=20)
 
 
 class Regulations_with_Grades(models.Model):
@@ -31,8 +26,8 @@ class Subject_max_marks(models.Model):
     Brach_code=models.CharField(max_length=20)
     Max_marks=models.IntegerField()
     Max_Total_marks=models.IntegerField()
-    Min_marks=models.IntegerField()
-    Min_Total_marks=models.IntegerField()
+    Max_internal=models.IntegerField()
+    Min_external=models.IntegerField()
     Credit=models.IntegerField()
     Subject_type=models.IntegerField()
 
@@ -41,12 +36,12 @@ class Revaluation_copy(models.Model):
     Student_Name=models.CharField(max_length=250)
     Subject_code=models.CharField( max_length=50)
     Subject=models.CharField(max_length=200)
-    Internal_marks=models.IntegerField()
-    External_marks=models.IntegerField()
+    Internal_marks=models.IntegerField(null=True,blank=True)
+    External_marks=models.IntegerField(null=True,blank=True)
     Second_evaluation=models.IntegerField(null=True,blank=True)
     Third_evaluation=models.IntegerField(null=True,blank=True)
-    Credits=models.IntegerField()
-    Grades=models.CharField(max_length=2)
+    Credits=models.IntegerField(null=True,blank=True)
+    Grades=models.CharField(max_length=2,null=True,blank=True)
     Revaluation_Status=models.CharField(max_length=20)
 
     
